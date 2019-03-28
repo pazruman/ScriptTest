00ddba497d503178593ebc057918829ef0c73d78
pazruman <pazruman@gmail.com>
2019-03-28 11:36:34 +0900

    課題 訂正　ScriptTest

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -13,23 +13,19 @@ public class Test : MonoBehaviour
         // 配列の要素数のぶんだけ処理を繰り返す
         for (int i = 0; i < points.Length; i++)
         {
-            // 配列の要素が0以上の場合
-            if (points[i] >= 0)
-            {
+            
                 // 配列の要素を表示する
                 Debug.Log(points[i]);
             }
-        }
+        
 
         // 配列の要素数のぶんだけ処理を繰り返す
         for (int k = points.Length - 1; k >= 0; k--)
         {
-            // 配列の要素が0以上の場合
-            if (points[k] >= 0)
-            {
+            
                 // 配列の要素を表示する
                 Debug.Log(points[k]);
-            }
+            
 
         }
 
